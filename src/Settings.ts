5c7b5d4201cb13dd00919f43e40b783b5180cefd
rei <reimakesgames.dev@gmail.com>
2023-09-18 00:39:26 +0800

    scaling and preview

---
+++ src/Settings.ts
@@ -0,0 +1,7 @@
+const Settings = {
+	SCREEN_SIZE_X: 800,
+	SCREEN_SIZE_Y: 600,
+	FILL: false, // Should the canvas fill the screen? If false, the canvas will be centered and letterboxed.
+}
+
+export default Settings
---
+++ src/bootstrap.ts
@@ -0,0 +1,58 @@
+import Settings from "./Settings.js";
+
+let CanvasContainer = document.createElement('div');
+CanvasContainer.classList.add('canvas-container');
+
+let Canvas = document.createElement('canvas');
+Canvas.width = Settings.SCREEN_SIZE_X;
+Canvas.height = Settings.SCREEN_SIZE_Y;
+
+CanvasContainer.appendChild(Canvas);
+document.body.appendChild(CanvasContainer);
+
+function fitToScreen() {
+	if (Settings.FILL) {
+		CanvasContainer.style.width = '100%';
+		CanvasContainer.style.height = '100%';
+	} else {
+		const ratio = Settings.SCREEN_SIZE_X / Settings.SCREEN_SIZE_Y;
+		const windowRatio = window.innerWidth / window.innerHeight;
+		if (windowRatio > ratio) {
+			CanvasContainer.style.width = `${window.innerHeight * ratio}px`;
+			CanvasContainer.style.height = `${window.innerHeight}px`;
+		} else {
+			CanvasContainer.style.width = `${window.innerWidth}px`;
+			CanvasContainer.style.height = `${window.innerWidth / ratio}px`;
+		}
+	}
+	requestAnimationFrame(fitToScreen);
+}
+requestAnimationFrame(fitToScreen);
+
+let CanvasRenderer = Canvas.getContext('2d') as CanvasRenderingContext2D;
+if (!CanvasRenderer) {
+	throw new Error('Could not get 2D context');
+}
+
+
+let img = new Image();
+img.src = 'texture.png';
+img.onload = () => {
+	CanvasRenderer.fillStyle = '#1F1F1F';
+	CanvasRenderer.fillRect(0, 0, Settings.SCREEN_SIZE_X, Settings.SCREEN_SIZE_Y);
+	CanvasRenderer.drawImage(img, 0, 0);
+
+	CanvasRenderer.fillStyle = '#FFFFFF';
+	CanvasRenderer.font = '36px Ubuntu';
+	CanvasRenderer.textAlign = 'center';
+	CanvasRenderer.textBaseline = 'bottom';
+	CanvasRenderer.fillText('REIGNITE', Settings.SCREEN_SIZE_X / 2, Settings.SCREEN_SIZE_Y / 2);
+	CanvasRenderer.font = '12px Ubuntu';
+	CanvasRenderer.textBaseline = 'top';
+	CanvasRenderer.fillText('A TypeScript Game Engine by Rei', Settings.SCREEN_SIZE_X / 2, Settings.SCREEN_SIZE_Y / 2 + 4);
+	CanvasRenderer.strokeStyle = '#FFFFFF';
+	CanvasRenderer.beginPath();
+	CanvasRenderer.moveTo(Settings.SCREEN_SIZE_X/2 - 120, Settings.SCREEN_SIZE_Y / 2);
+	CanvasRenderer.lineTo(Settings.SCREEN_SIZE_X/2 + 120, Settings.SCREEN_SIZE_Y / 2);
+	CanvasRenderer.stroke();
+}
