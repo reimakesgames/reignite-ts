91d18cc36d8097a020af7099d67d6e2dc63af8b5
rei <reimakesgames.dev@gmail.com>
2023-10-26 00:13:25 +0800

    patch

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,3 +1,5 @@
 export { Matrix2d } from "./datatypes/Matrix2d"
 export { Matrix3d } from "./datatypes/Matrix3d"
 export { Transform } from "./datatypes/Transform"
+
+export { Vector3 } from "./datatypes/Vector3"
