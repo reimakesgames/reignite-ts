634749e43fec97adae559903707e0b64ce19590a
rei <reimakesgames.dev@gmail.com>
2023-12-28 20:27:25 +0800

    fix test scene loader json table

--- src/engine/modules/App.ts
+++ src/engine/modules/App.ts
@@ -25,112 +25,114 @@ export function setWindowTitle(title: string) {
 }
 
 export function main(context: CanvasRenderingContext2D) {
-	const scene = root.loadSceneFromJson(`{
-		"class": "Scene",
-		"properties": {
-			"name": "Scene1"
-		},
-		"children": [
-			{
-				"class": "Camera",
-				"properties": {
-					"name": "Camera1",
-					"FieldOfView": 70,
-					"Transform": {
-						"datatype": "Transform",
-						"value": {
-							"position": {
-								"datatype": "Vector3",
-								"value": [0, 0, 0]
-							},
-							"rotation": {
-								"datatype": "Matrix3d",
-								"value": [
-									[1, 0, 0],
-									[0, 1, 0],
-									[0, 0, 1]
-								]
-							}
-						}
-					}
-				},
-				"children": [
-					{
-						"class": "Camera",
-						"properties": {
-							"name": "Camera1",
-							"FieldOfView": 70,
-							"Transform": {
-								"datatype": "Transform",
-								"value": {
-									"position": {
-										"datatype": "Vector3",
-										"value": [0, 0, 0]
-									},
-									"rotation": {
-										"datatype": "Matrix3d",
-										"value": [
-											[1, 0, 0],
-											[0, 1, 0],
-											[0, 0, 1]
-										]
-									}
-								}
-							}
-						}
-					},
-					{
-						"class": "Camera",
-						"properties": {
-							"name": "Camera1",
-							"FieldOfView": 70,
-							"Transform": {
-								"datatype": "Transform",
-								"value": {
-									"position": {
-										"datatype": "Vector3",
-										"value": [0, 0, 0]
-									},
-									"rotation": {
-										"datatype": "Matrix3d",
-										"value": [
-											[1, 0, 0],
-											[0, 1, 0],
-											[0, 0, 1]
-										]
-									}
-								}
-							}
-						}
-					}
-				]
+	const scene = root.loadSceneFromJson(
+		JSON.stringify({
+			class: "Scene",
+			properties: {
+				name: "Scene1",
 			},
-			{
-				"class": "Camera",
-				"properties": {
-					"name": "Camera1",
-					"FieldOfView": 70,
-					"Transform": {
-						"datatype": "Transform",
-						"value": {
-							"position": {
-								"datatype": "Vector3",
-								"value": [0, 0, 0]
+			children: [
+				{
+					class: "Camera",
+					properties: {
+						name: "Camera1",
+						FieldOfView: 70,
+						Transform: {
+							datatype: "Transform",
+							value: [
+								{
+									datatype: "Vector3",
+									value: [0, 0, 0],
+								},
+								{
+									datatype: "Matrix3d",
+									value: [
+										[1, 0, 0],
+										[0, 1, 0],
+										[0, 0, 1],
+									],
+								},
+							],
+						},
+					},
+					children: [
+						{
+							class: "Camera",
+							properties: {
+								name: "Camera1",
+								FieldOfView: 70,
+								Transform: {
+									datatype: "Transform",
+									value: [
+										{
+											datatype: "Vector3",
+											value: [0, 0, 0],
+										},
+										{
+											datatype: "Matrix3d",
+											value: [
+												[1, 0, 0],
+												[0, 1, 0],
+												[0, 0, 1],
+											],
+										},
+									],
+								},
 							},
-							"rotation": {
-								"datatype": "Matrix3d",
-								"value": [
-									[1, 0, 0],
-									[0, 1, 0],
-									[0, 0, 1]
-								]
-							}
-						}
-					}
-				}
-			}
-		]
-	}`)
+						},
+						{
+							class: "Camera",
+							properties: {
+								name: "Camera1",
+								FieldOfView: 70,
+								Transform: {
+									datatype: "Transform",
+									value: [
+										{
+											datatype: "Vector3",
+											value: [0, 0, 0],
+										},
+										{
+											datatype: "Matrix3d",
+											value: [
+												[1, 0, 0],
+												[0, 1, 0],
+												[0, 0, 1],
+											],
+										},
+									],
+								},
+							},
+						},
+					],
+				},
+				{
+					class: "Camera",
+					properties: {
+						name: "Camera1",
+						FieldOfView: 70,
+						Transform: {
+							datatype: "Transform",
+							value: [
+								{
+									datatype: "Vector3",
+									value: [0, 0, 0],
+								},
+								{
+									datatype: "Matrix3d",
+									value: [
+										[1, 0, 0],
+										[0, 1, 0],
+										[0, 0, 1],
+									],
+								},
+							],
+						},
+					},
+				},
+			],
+		})
+	)
 	scene.currentCamera = scene.children[0] as Camera
 
 	// fix so that the console doesn't spam errors
